ff0df12b9249322606db918df459195c0b6657c9
Adam Weaver <adam.weaver@protected.net>
2025-11-06 00:13:49 +0000

    update datetime version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,11 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+//    versionCatalogs {
+//        create("libs") {
+//            from(files("gradle/libs.versions.toml"))
+//        }
+//    }
     @Suppress("UnstableApiUsage")
     repositories {
         google()
